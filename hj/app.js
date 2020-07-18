01525289c835eb1ce065a742050e05e57555aebf
HyeonDKIM <aquaifnt7@gmail.com>
2020-07-18 23:50:34 +0900

    basic main page

--- hj/app.js
+++ hj/app.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+const fs = require('fs')
+
+app.use(express.static(__dirname + '/'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/', function(req, res){
-    res.send('hello, world!')
+    fs.readFile("main.html", function(err, data){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(data);
+    });
 });
 
 app.listen(3100, function(){
-    console.log('3000 port open for test server')
+    console.log('3100 port open for test server')
 });
