96b5c58e3fe64dc5a6d09e2bc32aa4e8a8cafcf3
HyeonDKIM <aquaifnt7@gmail.com>
2020-07-23 18:52:08 +0900

    make letters length more than 1

--- js/resources.js
+++ js/resources.js
@@ -1,19 +1,13 @@
 var GetLetter = []
 
-function WriteToFile(LetterData) {
-    var fso = CreateObject("Scripting.FileSystemObject"); 
-    var s   = fso.CreateTextFile("letters.txt", True);
- 
-    var toWrite = LetterData;
- 
-    s.writeline(toWrite);
-    s.Close();
- }
-
 function GetLetterByUser(data){
     var tempGet = document.getElementById('GetLetter').value;
     if(tempGet==""){
         alert("공백을 입력하셨습니다!");
+        return false;
+    }
+    else if(tempGet.length==1){
+        alert("두 글자 이상으로 입력해주세요!");
         return false;
     }
     GetLetter.push(tempGet);
